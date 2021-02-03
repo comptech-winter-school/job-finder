c43650dfd0a281f9e93e713a40d8d99483422109
Nikita <vipletun@gmail.com>
2021-02-03 22:26:37 +0300

    TEST

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, Pickl
 from api import start, enter_the_text, get_k_items
 
 if __name__ == '__main__':
-    ACCESS_TOKEN = '1662109088:AAE0QtQTQNBnzgWMueHwnKtZ6w8STAlMkK8'
+    ACCESS_TOKEN = os.environ['TOKEN']
     updater = Updater(token=ACCESS_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler('start', start))
     dispatcher.add_handler(CommandHandler('info', start))
     dispatcher.add_handler(MessageHandler(Filters.text('Вакансия'), enter_the_text))
     dispatcher.add_handler(MessageHandler(Filters.text, get_k_items))
-    dispatcher.add_handler(MessageHandler(Filters.text('Следующая'), get_k_items))
+
     updater.start_polling()
