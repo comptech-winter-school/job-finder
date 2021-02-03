2cd4f5bd2c365090e1a384a51ec63d5520ba09c2
Kruglikova Veronika Sergeevna <vkruglikova@ozon.ru>
2021-02-03 18:55:14 +0300

    add tfidf

--- models/Embedder.py
+++ models/Embedder.py
@@ -1,5 +1,38 @@
 from models.base_models import Embedder
+from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
+import nltk
+import re
+import unicodedata
+
+
+nltk.download("stopwords")
+from nltk.corpus import stopwords
+stoplist = stopwords.words("russian")
+
+def preproc(text: str):
+    """
+        Удаление юникодов типа \xa0 - неразрывный пробел
+        Удаление ссылок на сайты, на почту, на телеграм, удаление смайликов вида :smile:, удаление \n, \t, \r
+        Удаление пунктуации
+        Схлопывание двойных пробелов
+    """
+    pattern_html = r'<.*?>' 
+    pattern_smile = r':\w+?-?\w+:'
+    pattern_contact = r'\S*@\S+'
+    pattern_nt = r'[\n\t\r*]'
+    pattern_phone = r'(?:\d{9,11})?(?:\d *\(*\d{3}\)* *\d{3} *\d{2} *\d{2})?(?:\d\-*\d{3}\-*\d{3}\-*\d{2}\-*\d{2})?'
+    clean_text = unicodedata.normalize("NFKD", str(text))
+    clean_text = re.sub(pattern_html, r'', clean_text)
+    clean_text = re.sub(pattern_smile, r'', clean_text)
+    clean_text = re.sub(pattern_contact, r'', clean_text)
+    clean_text = re.sub(pattern_nt, r' ', clean_text)
+    clean_text = re.sub(pattern_phone, r'', clean_text)
+    punct = '[!"#$%&()*\+,\.:;<=>`{|}~„“«»†*‘’•·]'
+    clean_text = re.sub(punct, r'', clean_text)
+    clean_text = re.sub(r'\s+', ' ', clean_text)
+    return clean_text
+
 
 class RandomEmbedder(Embedder):
     def __init__(self):
@@ -28,3 +61,49 @@ class RandomEmbedder(Embedder):
                               [0.22196722, 0.88154772],
                               [0.07235152, 0.24377389]]).astype('float32')
         return self.embs
+    
+    def fit(self, texts):
+        pass
+
+    def save(self, output_path: str):
+        pass
+
+    def load(self, input_path: str):
+        pass
+
+
+class TfidfEmbedder(Embedder):
+    def __init__(self, max_features: int = 100):
+        self.vectorizer = TfidfVectorizer(max_features=max_features,
+                                    lowercase=True,
+                                    stop_words=stoplist)
+
+    def embedding(self, text):
+        return self.vectorizer.transform([text])
+
+    def fit(self, texts):
+        self.vectorizer.fit(texts)
+
+    def transform(self, text: list):
+        return self.vectorizer.transform([text])
+
+    def save(self, output_path: str):
+        if output_path[-9:] != '.embedder':
+            output_path += '.embedder'
+        try:
+            pickle.dump(self.vectorizer , open(output_path, "wb"))
+        except IOError as e:
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        except: 
+            print("Unexpected error")
+
+    def load(self, input_path: str):
+        if output_path[-9:] != '.embedder':
+            output_path += '.embedder'
+        try:
+            self.vectorizer = pickle.load(open(input_path, "rb"))
+        except IOError as e:
+            print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        except: 
+            print("Unexpected error")
+            
